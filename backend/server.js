1d5cf5250fa1654c665a62e668e7aa8a5f36e00d
Doven <doven.obertinca@gmail.com>
2022-05-11 10:51:20 +0200

    error handler and controllers done

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// to send json data to server
+app.use(express.json());
+
+// to send urlencoded
+app.use(express.urlencoded({ extended: false }));
+
+// to use errorHandler
+app.use(errorHandler);
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
